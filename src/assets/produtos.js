bc3c2a48c890ea9ceeaba05c76c3940e571ff5c5
karinnaJonhs <karendias.842@gmail.com>
2022-10-19 17:50:11 +0100

    finalização imagens para o populate

--- src/assets/produtos.js
+++ src/assets/produtos.js
@@ -15,55 +15,55 @@ export const products = [
     }, {
         "pathToImage": "/img/4.png",
         "name": "Disco Chair",
-        "price": ""
+        "price": "3000€"
     }, {
         "pathToImage": "/img/5.png",
-        "name": "",
-        "price": ""
+        "name": "Can Table Chair",
+        "price": "35000€"
     }, {
         "pathToImage": "/img/6.png",
-        "name": "",
-        "price": ""
+        "name": "Cardboard Sofa",
+        "price": "45000€"
     }, {
         "pathToImage": "/img/7.png",
-        "name": "",
-        "price": ""
+        "name": "Grass Bottle Mini Table",
+        "price": "7000€"
     }, {
         "pathToImage": "/img/8.png",
-        "name": "",
-        "price": ""
+        "name": "Drunk Table Wine",
+        "price": "12000€"
     }, {
         "pathToImage": "/img/9.png",
-        "name": "",
-        "price": ""
+        "name": "Wine Cap Armchair",
+        "price": "20000€"
     }, {
         "pathToImage": "/img/10.png",
-        "name": "",
-        "price": ""
+        "name": "Lampshade Colorful Cap Metalic ",
+        "price": "25000€"
     }, {
         "pathToImage": "/img/11.png",
-        "name": "",
-        "price": ""
+        "name": "Cap Box Inventary",
+        "price": "17000€"
     }, {
         "pathToImage": "/img/12.png",
-        "name": "",
-        "price": ""
+        "name": "Metalic Can Mirror",
+        "price": "6500€"
     }, {
         "pathToImage": "/img/13.png",
-        "name": "",
-        "price": ""
+        "name": "Metalic Bowl",
+        "price": "5000€"
     }, {
         "pathToImage": "/img/14.png",
-        "name": "",
-        "price": ""
+        "name": "Barrel Cradle Rustic",
+        "price": "35000€"
     }, {
         "pathToImage": "/img/15.png",
-        "name": "",
-        "price": ""
+        "name": "Book Moderm Sofa",
+        "price": "22000€"
     }, {
         "pathToImage": "/img/16.png",
-        "name": "",
-        "price": ""
+        "name": "Chair Table Set",
+        "price": "50000€"
     }, {
         "pathToImage": "/img/17.png",
         "name": "",
