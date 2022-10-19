8f9c95ef67223e47f1c3dca13dee8d0787fac904
Ariane Guimarães <arianesssrezende@gmail.com>
2022-10-20 00:42:38 +0100

    Artwork page changes.

--- pages/artworks.jsx
+++ pages/artworks.jsx
@@ -31,182 +31,184 @@ export default function Artworks() {
   return (
     <div className={styles.main}>
       <div className={styles.body}>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Sofa Brass Red</h3>
-          <img src="/img/1.png" />
+          <img src="/img/1.png" className={styles.thumbnail} />
           <p>
             Price: 20000€ <br />
             Code: 8r4ss
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Lampshade Cap Purple and White</h3>
-          <img src="/img/2.png" />
+          <img src="/img/2.png" className={styles.thumbnail} />
           <p>
             Price: 5000€ <br />
             Code: pl4mp
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Amrchair Single Tennis Balls</h3>
-          <img src="/img/3.png" />
+          <img src="/img/3.png" className={styles.thumbnail} />
           <p>
             Price: 1000€ <br />
             Code: t3nn1s
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Vinyl Chair</h3>
-          <img src="/img/4.png" />
+          <img src="/img/4.png" className={styles.thumbnail} />
           <p>
             Price: 3000€ <br />
             Code: v1ny1ch
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Can Table Chair</h3>
-          <img src="/img/5.png" />
+          <img src="/img/5.png" className={styles.thumbnail} />
           <p>
             Price: 35000€ <br />
             Code: c4nt4bl3
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Cardboard Sofa</h3>
-          <img src="/img/6.png" />
+          <img src="/img/6.png" className={styles.thumbnail} />
           <p>
             Price: 45000€ <br />
             Code: c4rds0f4
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Grass Bottle Mini Table</h3>
-          <img src="/img/7.png" />
+          <img src="/img/7.png" className={styles.thumbnail} />
           <p>
             Price: 7000€ <br />
             Code: gr455b0tt1e
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Drunk Table Wine</h3>
-          <img src="/img/8.png" />
+          <img src="/img/8.png" className={styles.thumbnail} />
           <p>
             Price: 12000€ <br />
             Code: dr8nkt4ble
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Cork Armchair</h3>
-          <img src="/img/9.png" />
+          <img src="/img/9.png" className={styles.thumbnail} />
           <p>
             Price: 20000€ <br />
             Code: c0rkch41r
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Lampshade Colourful Metallic Caps</h3>
-          <img src="/img/10.png" />
+          <img src="/img/10.png" className={styles.thumbnail} />
           <p>
             Price: 25000€ <br />
             Code: m3t4ll1c4ps
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Cap Basket</h3>
-          <img src="/img/11.png" />
+          <img src="/img/11.png" className={styles.thumbnail} />
           <p>
             Price: 17000€ <br />
             Code: c4pb4sk37
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Metallic Can Mirror</h3>
-          <img src="/img/12.png" />
+          <img src="/img/12.png" className={styles.thumbnail} />
           <p>
             Price: 6500€ <br />
             Code: c4nm1rr0r
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Metallic Bowl</h3>
-          <img src="/img/13.png" />
+          <img src="/img/13.png" className={styles.thumbnail} />
           <p>
             Price: 5000€ <br />
             Code: m3t41b0wl
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Barrel Rustic Cradle</h3>
-          <img src="/img/14.png" />
+          <img src="/img/14.png" className={styles.thumbnail} />
           <p>
             Price: 35000€ <br />
             Code: m3t4lcr4dl3
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Modern Book Sofa</h3>
-          <img src="/img/15.png" />
+          <img src="/img/15.png" className={styles.thumbnail} />
           <p>
             Price: 22000€ <br />
             Code: b00ks0f4
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Chair & Table Set</h3>
-          <img src="/img/16.png" />
+          <img src="/img/16.png" className={styles.thumbnail} />
           <p>
             Price: 50000€ <br />
             Code: t4bl3ch41rs3t
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Balcony Set</h3>
-          <img src="/img/17.png" />
+          <img src="/img/17.png" className={styles.thumbnail} />
           <p>
             Price: 15000€ <br />
             Code: b4lc0ny
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Pipe Coffee Table</h3>
-          <img src="/img/18.png" />
+          <img src="/img/18.png" className={styles.thumbnail} />
           <p>
             Price: 8000€ <br />
             Code: p1p3c0ff33
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Ring Chandelier</h3>
-          <img src="/img/19.png" />
+          <img src="/img/19.png" className={styles.thumbnail} />
           <p>
             Price: 3000€ <br />
             Code: r1n6ch4nd
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Colourful Cap Mirror</h3>
-          <img src="/img/20.png" />
+          <img src="/img/20.png" className={styles.thumbnail} />
           <p>
             Price: 7000€ <br />
             Code: c0l0urc4pm1r
           </p>
         </div>
-        <div className="artWork">
+        <div className={styles.artWork}>
           <h3>Pallet Bed</h3>
-          <img src="/img/21.png" />
+          <img src="/img/21.png" className={styles.thumbnail} />
           <p>
             Price: 50000€ <br />
             Code: p4ll3tb3d
           </p>
         </div>
       </div>
-      <div className="reservationForm">
+      <hr/>
+      <div className={styles.reservationForm}>
         <h2>
-          If you want to reserve one of these art pieces for yourself, you can!
+          If you want to reserve one of these art pieces for yourself, you can! <br/>
           All you have to do is fill this small form!
         </h2>
-        <form className="makeReservation">
+        <form >
           <input
+            className={styles.input}
             placeholder="Name"
             type="text"
             value={name}
@@ -214,6 +216,7 @@ export default function Artworks() {
           />
           <br />
           <input
+            className={styles.input}
             placeholder="Email"
             type="text"
             value={email}
@@ -221,6 +224,7 @@ export default function Artworks() {
           />
           <br />
           <input
+            className={styles.input}
             placeholder="Art Code"
             type="text"
             value={code}
