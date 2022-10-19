c2e87066ccc4f988efed3f7968252cddb00d642f
Anacleto1409 <112070226+Anacleto1409@users.noreply.github.com>
2022-10-19 23:09:29 +0100

    Update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -3,9 +3,9 @@ export default function MenuSobre() {
   return (
     <div className="mainAbout">
       <img className="creatorsPic" src="" />
-      <p>Created by:</p>
+      <p>About Me:</p>
       <ul>
-        <li>Fábio Giordano</li>
+        <li>My name is Fabio Giordano and I created my brand because I believe that the world would be a better place if everyone recycled what they don't use, so that the world would be better for the next generations. </li>
         <p></p>
       </ul>
     </div>
