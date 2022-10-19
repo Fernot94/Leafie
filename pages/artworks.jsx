77ff031bfbe305c457fc2a6e1c30d9769bb6afeb
Fernot94 <fernoo94@hotmail.com>
2022-10-19 18:49:25 +0100

    fetches

--- pages/artworks.jsx
+++ pages/artworks.jsx
@@ -2,29 +2,25 @@ import { useState } from "react";
 import { useEffect } from "react";
 
 export default function Artworks() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState();
   const [link, setLink] = useState("");
 
-  //   useEffect(() => {
-  //     const options = {
-  //       method: "GET",
-  //     };
-  //     fetch("/api/populate", options)
-  //       .then((response) => response.json())
-  //       .then((response) => setItems(response.tables))
-  //       .catch((err) => console.error(err));
-  //   }, []);
+  useEffect(() => {
+    const options = {
+      method: "GET",
+    };
+
+    fetch("/api/product", options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .then((response) => setItems(response))
+      .catch((err) => console.error(err));
+  }, []);
 
   return (
     <div className="mainArtworks">
       <div className="artPieces">
-        <h3>Sofa Brass Red</h3>
-        <img src="/img/1.png" />
-        <p>
-          Description <br />
-          Price: 20000€ <br />
-          ID: 8r4ss
-        </p>
+        <p>{items[0]._id}</p>
       </div>
     </div>
   );
