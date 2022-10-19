a2ae1e8e8be7d1f65d4ca282088ef61dab9201ad
Fernot94 <fernoo94@hotmail.com>
2022-10-19 13:49:38 +0100

    added admin

--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,9 +1,26 @@
 import { useState } from "react";
 
 export default function Admin() {
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
+
+  const handleClick = () => {
+    setCode(input);
+  };
   return (
     <div>
-      <p>Placeholder</p>
+      <div>
+        <p>Enter Artist Backoffice Code</p>
+        <div className="inputBox">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <br />
+          <button onClick={handleClick}>Log In</button>
+        </div>
+      </div>
     </div>
   );
 }
