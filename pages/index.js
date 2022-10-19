463fd8cc801133527cef4b90c9489c55d7b89308
OverLord966 <daabea2001@gmail.com>
2022-10-19 13:14:00 +0100

    Added some useful

--- pages/index.js
+++ pages/index.js
@@ -2,39 +2,22 @@ import { useState } from 'react'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link';
 
-const bunkers = [
-  {
-    name: "VIVOS EUROPA ONE",
-    price: "2.2 M",
-    location: {
-      city: "Nantes",
-      country: "France"
-    },
-    tags: ["biochemical", "cyber"]
-  },
-]
 
 function Header() {
   return (
-    <div className={styles.header}
-      style={{ backgroundImage: "url(/header.jpg)" }}>
+    <div className={styles.header}>
       <img src={"./logo.png"} className={styles.logo} />
-      <div className="textoHeader"><h1>LOOKING TO<br/>
-        SURVIVE IN STYLE?</h1>
-      <h5>A.BUNKER is the best solutions to enjoy the end of days.</h5></div>
 
-
-      
 
       <div className={styles.login}>
-        <Link href={'/Login'}><button className={styles.button_login}>Login</button></Link>
-        </div>
-      <div className={styles.login}>
-        <Link href={'/Singup'}><button className={styles.button_singup}>SignUp</button></Link>
-        
+        <Link href={'/Login'}><button className={styles.button_login}>Exposição</button></Link>
       </div>
       <div className={styles.login}>
-      <Link href={'/Perfil'}><button className={styles.button_perfil}>Perfil</button></Link>
+        <Link href={'/Singup'}><button className={styles.button_singup}>SignUp</button></Link>
+
+      </div>
+      <div className={styles.login}>
+        <Link href={'/Perfil'}><button className={styles.button_perfil}>Perfil</button></Link>
       </div>
 
     </div>
@@ -46,12 +29,9 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Header></Header>
-
-      <div className={styles.selection}><h2>Our selection</h2></div>
+      <div className={styles.leafie}><h1>LEAFIE</h1></div>
+      <div className={styles.selection}><h2>Acerca da exposição</h2></div>
       <div className={styles.body}>
-        <BunkerCard data={bunkers[0]}></BunkerCard>
-        <BunkerCard data={bunkers[1]}></BunkerCard>
-        <BunkerCard data={bunkers[2]}></BunkerCard>
 
       </div>
 
@@ -94,10 +74,53 @@ export default function Home() {
 }
 
 /*
-<div className={styles.searchBar}>
-        <div className={styles.search}>
-          <SearchIcon />
-          <input type="text" placeholder="Search.." />
+
+const bunkers = [
+  {
+    imagePath: "/Bunker1/1a.jpg",
+    name: "VIVOS EUROPA ONE",
+    price: "2.2 M",
+    location: {
+      city: "Nantes",
+      country: "France"
+    },
+    tags: ["biochemical", "cyber"]
+  }
+]
+function BunkerCard({ data }) {
+  let path = ""
+  let bunker = ""
+  if(data.imagePath == "/Bunker1/1a.jpg"){
+    path = data.imagePath
+    bunker = "/Bunkers/Bunker"
+  }
+  else if(data.imagePath == "/Bunker2/3a.jpg"){
+    path = data.imagePath
+    bunker = "/Bunkers/Bunker1"
+  }
+  if(data.imagePath == "/Bunker3/4A.jpg"){
+    path = data.imagePath
+    bunker = "/Bunkers/Bunker2"
+  }
+  return (
+    <div>
+      <div className={styles.upper}>
+        <div className={styles.BunkerImage}>
+          <Link href={bunker}><img src={data.imagePath} /></Link>
         </div>
       </div>
+      <div className={styles.lower}>
+        <div className={styles.Dados}>
+          <div className={styles.Nomes}>{data.name}</div>
+          <div className={styles.Preços}>{data.location.city}, {data.location.country}</div>
+          <div className={styles.Local}>{data.price} €</div>
+        </div>
+        <div className={styles.tags}>
+          {data.tags.map(t => <img src={`/Icones_Catastrofes/${t}.png`}></img>)}
+        </div>
+      </div>
+    </div>
+
+  )
+}
 */
