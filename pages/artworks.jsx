3d0d1da0cd193c00da9eb75b649a71f25e621c63
Fernot94 <fernoo94@hotmail.com>
2022-10-19 17:13:00 +0100

    artworks

--- pages/artworks.jsx
+++ pages/artworks.jsx
@@ -2,9 +2,13 @@ export default function Artworks() {
   return (
     <div className="mainArtworks">
       <div className="artPieces">
-        <img src="/img/1.png" alt="Royal Straight Flush" />
-        <h3></h3>
-        <p></p>
+        <h3>Sofa Brass Red</h3>
+        <img src="/img/1.png" />
+        <p>
+          Description <br />
+          Price: 20000€ <br />
+          ID: 8r4ss
+        </p>
       </div>
     </div>
   );
