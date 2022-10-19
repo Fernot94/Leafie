e7df53c4c544edb015e83b8473eff07560b6e283
Anacleto1409 <112070226+Anacleto1409@users.noreply.github.com>
2022-10-19 23:22:48 +0100

    homem

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 export default function MenuSobre() {
   return (
     <div className="mainAbout">
-      <img className="creatorsPic" src="" />
+      <img className="creatorsPic" src="/img/homem.png" />
       <p>About Me:</p>
       <ul>
         <li>My name is Fabio Giordano and I created my brand because I believe that we would all live better if everyone recycled what they don't use so that the world would be better for the next generations.  </li>
