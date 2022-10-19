c47df3cbf88a4d972fe0d219c8f4fcef3f1b96ef
karinnaJonhs <karendias.842@gmail.com>
2022-10-19 17:11:17 +0100

    nao tah dando

---
+++ src/assets/produtos.js
@@ -0,0 +1,87 @@
+export const products = [
+    {
+        "pathToImage": "/img/1.png",
+        "name": "Sofa Brass Red",
+        "price": "20000€"
+    }, {
+        "pathToImage": "/img/2.png",
+        "name": "Lampshade Cap Purple and White",
+        "price": "5000€"
+    }, {
+        "pathToImage": "/img/3.png",
+        "name": "Poltrona Solteiro Bolinha de Tênis",
+        "price": ""
+    }, {
+        "pathToImage": "/img/4.png",
+        "name": "Armchair",
+        "price": ""
+    }, {
+        "pathToImage": "/img/5.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/6.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/7.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/8.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/9.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/10.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/11.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/12.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/13.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/14.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/15.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/16.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/17.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/18.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/19.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/20.png",
+        "name": "",
+        "price": ""
+    }, {
+        "pathToImage": "/img/21.png",
+        "name": "",
+        "price": ""
+    }
+]
