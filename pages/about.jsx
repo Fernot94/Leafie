1decde8925ccd0b9c09a75a436af0ac0dce0abdc
Anacleto1409 <112070226+Anacleto1409@users.noreply.github.com>
2022-10-19 22:59:32 +0100

    Update about.jsx

---
+++ pages/about.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+export default function MenuSobre() {
+  return (
+    <div className="mainAbout">
+      <img className="creatorsPic" src="" />
+      <p>Created by:</p>
+      <ul>
+        <li>Fábio Giordano</li>
+        <p></p>
+      </ul>
+    </div>
+  );
+}
