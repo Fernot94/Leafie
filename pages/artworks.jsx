97dc7c1f255bf381c1106f6eb66f6b6b395ecd71
Fernot94 <fernoo94@hotmail.com>
2022-10-19 17:08:50 +0100

    added artworks file

--- pages/artworks.jsx
+++ pages/artworks.jsx
@@ -1,1 +1,11 @@
-export default function Artworks() {}
+export default function Artworks() {
+  return (
+    <div className="mainArtworks">
+      <div className="artPieces">
+        <img src="/img/1.png" alt="Royal Straight Flush" />
+        <h3></h3>
+        <p></p>
+      </div>
+    </div>
+  );
+}
