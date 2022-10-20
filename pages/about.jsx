1c08860fc2c2053e1bc76cbfe3ec9a9e0dd036c9
OverLord966 <daabea2001@gmail.com>
2022-10-20 11:03:31 +0100

    wtf

--- pages/about.jsx
+++ pages/about.jsx
@@ -10,6 +10,9 @@ export default function MenuSobre() {
       <p>My name is Fábio Giordano and I created my brand because I believe that we would all live better if everyone recycled what they don't use so that the world would be better for the next generations.</p><p>To achieve my goal, I have partnered with the Portuguese marine litter association, the association removes rubbish from the sea and delivers it so that it can be used by me to create unique and sustainable works of art.</p> <p> More recently I have partnered with World Wide Fund for Nature which works in the field of wilderness preservation and reducing human impact on the environment.</p>
       I hope you enjoy the artwork.  
 
+
+
+      
     </div>
   );
 }
